6acb488b1433638bb51d3cedbe87452d97baa4e0
DESKTOP-9JG8FU1\Karlin <karlinkd010@gmail.com>
2021-06-21 16:46:10 -0500

    Agregar archivos de proyecto.

---
+++ ToqueDeCampana-Practica/Models/Clase/ViewImage.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToqueDeCampana_Practica.Models
+{
+    public class ViewImage
+    {
+
+        public int id { get; set; }
+        public string VchMatricula { get; set; }
+        public IFormFile imagename { get; set; }
+    }
+}
